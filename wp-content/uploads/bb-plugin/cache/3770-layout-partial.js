ab951589c5b6ae36d57bb0f42131115acf6f32ed
bryanseah234 <bryanseah234@gmail.com>
2020-11-05 19:57:33 +0800

    Add files via upload

---
+++ wp-content/uploads/bb-plugin/cache/3770-layout-partial.js
@@ -0,0 +1,2 @@
+
+(function($){$(function(){new FLBuilderPostGrid({id:'58fc3562acfc3',layout:'feed',pagination:'none',postSpacing:'60',postWidth:'300',matchHeight:0});});})(jQuery);
---
+++ wp-content/uploads/bb-plugin/cache/3851-layout-partial.js
@@ -0,0 +1,2 @@
+
+(function($){$(function(){new FLBuilderPostGrid({id:'590b2d6319402',layout:'feed',pagination:'none',postSpacing:'60',postWidth:'300',matchHeight:0});});})(jQuery);
